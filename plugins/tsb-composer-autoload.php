7f63078f0b0d6a363172dfba465898af76bdcc74
Pedro Duarte <xipasduarte@users.noreply.github.com>
2025-08-01 19:16:04 +0100

    Add support for vendor folder in

--- plugins/tsb-composer-autoload.php
+++ plugins/tsb-composer-autoload.php
@@ -9,10 +9,19 @@
  * License:     GPL-2.0+
  */
 
-// Check if the composer autoload file exists in WordPress root or above, for sub-folder installs.
+/**
+ * Check if the composer autoload file exists in one of 3 places and require it.
+ * 1. The WordPress root
+ * 2. One folder above the WordPress root (sub-folder installs)
+ * 3. Inside the `wp-content` folder
+ */
 if ( file_exists( ABSPATH . '/vendor/autoload.php' ) ) {
 	require_once ABSPATH . '/vendor/autoload.php';
 
 } else if ( file_exists( ABSPATH . '../vendor/autoload.php' ) ) {
 	require_once ABSPATH . '../vendor/autoload.php';
+
+} else if ( file_exists( ABSPATH . '/wp-content/vendor/autoload.php' ) ) {
+	require_once ABSPATH . '/wp-content/vendor/autoload.php';
 }
+
