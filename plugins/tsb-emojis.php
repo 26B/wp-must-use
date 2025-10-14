ceafad932a725b74af4efc5df30c42eaae5a8a9c
Pedro Duarte <xipasduarte@users.noreply.github.com>
2025-10-14 16:56:26 +0100

    Add comments to tsb-emojis.php removals

--- plugins/tsb-emojis.php
+++ plugins/tsb-emojis.php
@@ -18,10 +18,18 @@ namespace TSB\WP\MUPlugin\Emojis;
  * @return void
  */
 function disable_emojis() {
+
+	// Remove the Emoji detection script.
 	remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+
+	// Remove inline Emoji detection script.
 	remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+
+	// Remove Emoji-related styles from front end and back end.
 	remove_action( 'wp_print_styles', 'print_emoji_styles' );
 	remove_action( 'admin_print_styles', 'print_emoji_styles' );
+
+	// Remove Emoji-to-static-img conversion.
 	remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
 	remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );
 	remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
